1b933fa001ab4b4abed5a60f20fe463d7ef604c2
Felipe Moreira <felipe0365@gmail.com>
2024-10-14 20:58:36 -0300

    Referenciando os posts do usuario

--- src/main/java/br/com/compassuol/workshopmongo/domain/User.java
+++ src/main/java/br/com/compassuol/workshopmongo/domain/User.java
@@ -1,12 +1,20 @@
 package br.com.compassuol.workshopmongo.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+@Getter
+@Setter
 @Document(collection = "user")
 public class User implements Serializable {
     @Serial
@@ -17,36 +25,16 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @Setter(AccessLevel.NONE)
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
     public User() {
     }
 
     public User(String id, String name, String email) {
         this.id = id;
         this.name = name;
-        this.email = email;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
     }
 
