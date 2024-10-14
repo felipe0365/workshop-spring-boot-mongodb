ef3c57fa8b829b5636047f648253159242fe4351
Felipe Moreira <felipe0365@gmail.com>
2024-10-13 23:06:26 -0300

    Criando entity post com User aninhado

---
+++ src/main/java/br/com/compassuol/workshopmongo/domain/Post.java
@@ -0,0 +1,29 @@
+package br.com.compassuol.workshopmongo.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Document
+public class Post implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+    private User author;
+
+}
