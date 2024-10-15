b3b0b0bf5b43ff90cc6bcc4380defcca8fcb78dd
Felipe Moreira <felipe0365@gmail.com>
2024-10-15 08:58:05 -0300

    Consulta somples com query methods

--- src/main/java/br/com/compassuol/workshopmongo/resources/PostsResource.java
+++ src/main/java/br/com/compassuol/workshopmongo/resources/PostsResource.java
@@ -1,6 +1,7 @@
 package br.com.compassuol.workshopmongo.resources;
 
 import br.com.compassuol.workshopmongo.domain.Post;
+import br.com.compassuol.workshopmongo.resources.util.URL;
 import br.com.compassuol.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/posts ")
+@RequestMapping(value = "/posts")
 public class PostsResource {
 
     @Autowired
@@ -26,5 +27,12 @@ public class PostsResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",defaultValue = "") String text) {
+        text = URL.decodeParams(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok(list);
     }
 }
--- src/main/java/br/com/compassuol/workshopmongo/services/PostService.java
+++ src/main/java/br/com/compassuol/workshopmongo/services/PostService.java
@@ -23,4 +23,8 @@ public class PostService {
     public List<Post> findAll() {
         return postRepository.findAll();
     }
+
+    public List<Post> findByTitle(String title) {
+        return postRepository.findByTitleContainingIgnoreCase(title);
+    }
 }
